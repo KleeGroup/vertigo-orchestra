3b6f2f7caafb11e842cabe192e3a95124d49c6e7
phlppchrtn <pchretien55@gmail.com>
2016-07-25 16:59:08 +0200

    entity

--- vertigo-orchestra-impl/src/main/javagen/io/vertigo/orchestra/domain/referential/TriggerType.java
+++ vertigo-orchestra-impl/src/main/javagen/io/vertigo/orchestra/domain/referential/TriggerType.java
@@ -2,14 +2,14 @@ package io.vertigo.orchestra.domain.referential;
 
 import io.vertigo.dynamo.domain.stereotype.DtDefinition;
 import io.vertigo.dynamo.domain.stereotype.Field;
-import io.vertigo.dynamo.domain.model.DtObject;
+import io.vertigo.dynamo.domain.model.Entity;
 import io.vertigo.dynamo.domain.util.DtObjectUtil;
 /**
  * Attention cette classe est générée automatiquement !
  * Objet de données TriggerType
  */
 @DtDefinition
-public final class TriggerType implements DtObject {
+public final class TriggerType implements Entity {
 
 	/** SerialVersionUID. */
 	private static final long serialVersionUID = 1L;
