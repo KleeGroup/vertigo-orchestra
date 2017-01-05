bf4d11a072dc0c8d2d0971a73b9958083d20c42a
phlppchrtn <pchretien55@gmail.com>
2017-01-05 17:34:28 +0100

    + simple

--- vertigo-orchestra-impl/src/main/java/io/vertigo/orchestra/impl/process/execution/ProcessExecutorPlugin.java
+++ vertigo-orchestra-impl/src/main/java/io/vertigo/orchestra/impl/process/execution/ProcessExecutorPlugin.java
@@ -1,35 +1,15 @@
 package io.vertigo.orchestra.impl.process.execution;
 
-import java.util.Optional;
-
 import io.vertigo.lang.Plugin;
-import io.vertigo.orchestra.definition.ProcessDefinition;
 import io.vertigo.orchestra.definition.ProcessType;
-import io.vertigo.orchestra.process.execution.ActivityExecutionWorkspace;
-import io.vertigo.orchestra.process.execution.ExecutionState;
+import io.vertigo.orchestra.process.execution.ProcessExecutor;
 
 /**
  * Plugin d'execution des processus orchestra.
  * @author mlaroche
  *
  */
-public interface ProcessExecutorPlugin extends Plugin {
-
-	/**
-	 * @see io.vertigo.orchestra.process.execution.ProcessExecutor#execute(ProcessDefinition, Optional)
-	 */
-	void execute(ProcessDefinition processDefinition, Optional<String> initialParams);
-
-	/**
-	 * @see io.vertigo.orchestra.process.execution.ProcessExecutor#endPendingActivityExecution(Long, String, ExecutionState)
-	 */
-	void endPendingActivityExecution(Long activityExecutionId, String token, ExecutionState state);
-
-	/**
-	 * @see io.vertigo.orchestra.process.execution.ProcessExecutor#setActivityExecutionPending(Long, ActivityExecutionWorkspace)
-	 */
-	void setActivityExecutionPending(Long activityExecutionId, ActivityExecutionWorkspace workspace);
-
+public interface ProcessExecutorPlugin extends ProcessExecutor, Plugin {
 	/**
 	 * Retourne le type de processus géré par le plugin
 	 * @return le type de processus géré
