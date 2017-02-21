cef6eb2ed71c0ce6838f7929c67522f80147cc7a
phlppchrtn <pchretien55@gmail.com>
2017-02-21 17:31:05 +0100

    mda

---
+++ vertigo-orchestra-impl/src/main/java/io/vertigo/orchestra/OrchestraGenerator.java
@@ -0,0 +1,9 @@
+package io.vertigo.orchestra;
+
+import io.vertigo.studio.tools.NameSpace2Java;
+
+public class OrchestraGenerator {
+	public static void main(final String[] args) {
+		NameSpace2Java.main(new String[] { "/mda/build-mda.properties" });
+	}
+}
